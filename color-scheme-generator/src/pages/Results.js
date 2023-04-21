e9185672a424002ee886cadd36ac772b8a40e593
Sara Harder <harders@oregonstate.edu>
2023-04-21 21:42:54 +0200

    Implemented functionality! Results now displayed

--- color-scheme-generator/src/pages/Results.js
+++ color-scheme-generator/src/pages/Results.js
@@ -8,51 +8,50 @@ import React from 'react';
 // import {useState} from "react"
 import {useHistory, useLocation} from "react-router-dom"
 
+function convertHex (num) {
+    let hex;
+    if (num == 10) hex = "A"
+    else if (num == 11) hex = "B"
+    else if (num == 12) hex = "C"
+    else if (num == 13) hex = "D"
+    else if (num == 14) hex = "E"
+    else if (num == 15) hex = "F"
+    else hex = String(num)
+    return hex
+}
+
 function ResultsPage () {
     const history = useHistory()
     const location = useLocation()
 
-    const colorList = location.state.colors
+    // retrieves the string of rgb vals, cutting off unneccessary first and last char
+    let colorString = location.state.colors
+    colorString = colorString.slice(1, colorString.length - 1)
 
     const send = () => {
         history.push({pathname: "/"})
     }
 
-    // creation of 6 random colors
-    const colors = [], rgbs = [], text = []
-    for (let r=0; r<6; r++) {
-        let color = "#", two = true, thr = 0, rgb = [], blk = false
-        for (let i=0; i<6; i++) {
-            // chooses 6 random numbers per color, from 1 to 16
-            let val = Math.floor(Math.random() * 16);
+    const rgbs = [], hexvals = [], text = []
+    // splits the string to retrieve 6 single strings, one for each color
+    const colorList = colorString.split("), (")
+    for (let color of colorList){
+        // splits the color's string and makes it an array of ints
+        color = color.split(", ")
+        let hex = "#", blk = false
+        for (let i in color){
+            color[i] = Number(color[i])
+            if (color[i] > 175) blk = true
 
-            // for every 2 numbers, multiplies first by 16
-            if (two) {
-                thr = val * 16
-                two = false
-            // adds second to first, creating 1 out of 3 rgb vals
-            } else {
-                rgb.push(thr+val)
-                if (thr+val > 175) blk = true
-                thr = 0
-                two = true
-            }
-
-            // for each random num, determines its hex val
-            if (val == 10) val = "A"
-            else if (val == 11) val = "B"
-            else if (val == 12) val = "C"
-            else if (val == 13) val = "D"
-            else if (val == 14) val = "E"
-            else if (val == 15) val = "F"
-            else val = String(val)
-            color = color.concat(val)
+            const hex2 = color[i] % 16
+            const hex1 = (color[i] - hex2) / 16
+            hex = hex.concat(convertHex(hex1))
+            hex = hex.concat(convertHex(hex2))
         }
-        // pushes calculated hex val and rgb vals
+        rgbs.push(color)
+        hexvals.push(hex)
         if (blk) text.push("black")
         else text.push("white")
-        colors.push(color)
-        rgbs.push(rgb)
     }
 
     return(
@@ -62,14 +61,14 @@ function ResultsPage () {
             <table>
                 <tbody>
                     <tr>
-                        {colors.slice(0, 3).map((color, i) => 
+                        {hexvals.slice(0, 3).map((color, i) => 
                         <td style={{"backgroundColor": color, "color": text[i]}} key={i}>
                             <div className='stats'>{color}
                             <br/>rgb({rgbs[i][0]}, {rgbs[i][1]}, {rgbs[i][2]})</div>
                         </td>)}
                     </tr>
                     <tr>
-                        {colors.slice(3, 6).map((color, i) => 
+                        {hexvals.slice(3, 6).map((color, i) => 
                         <td style={{"backgroundColor": color, "color": text[i+3]}} key={i}>
                             <div className='stats'>{color}
                             <br/>rgb({rgbs[i+3][0]}, {rgbs[i+3][1]}, {rgbs[i+3][2]})</div>
