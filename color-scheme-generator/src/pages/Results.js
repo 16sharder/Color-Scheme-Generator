1a488bfd6f685ceb8151d3c691f6576e0fec002b
Sara Harder <harders@oregonstate.edu>
2023-04-24 12:23:11 +0200

    Added UI buttons (no effect)

--- color-scheme-generator/src/pages/Results.js
+++ color-scheme-generator/src/pages/Results.js
@@ -61,25 +61,35 @@ function ResultsPage () {
         <>
             <h2>Here is your color scheme:</h2>
             <h4>Select a color to edit or discard it</h4>
-            <table>
+            <table className="colors">
                 <tbody>
                     <tr>
+                        <td><button>Modify Scheme</button></td>
                         {hexvals.slice(0, 3).map((color, i) => 
                         <td className="color" style={{"backgroundColor": color, "color": text[i]}} key={i}>
                             <div className='stats'>{color}
                             <br/>rgb({rgbs[i][0]}, {rgbs[i][1]}, {rgbs[i][2]})</div>
                         </td>)}
+                        <td><button>Hide Color Statistics</button></td>
                     </tr>
                     <tr>
+                        <td><button>Restore Original Colors</button></td>
                         {hexvals.slice(3, 6).map((color, i) => 
                         <td className="color" style={{"backgroundColor": color, "color": text[i+3]}} key={i}>
                             <div className='stats'>{color}
                             <br/>rgb({rgbs[i+3][0]}, {rgbs[i+3][1]}, {rgbs[i+3][2]})</div>
                         </td>)}
+                        <td><button>See Image Details</button></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button>Download Results</button></td>
+                        <td></td>
+                        <td><button onClick={() => history.push({pathname: "/"})}>Upload New Image</button></td>
+                        <td></td>
                     </tr>
                 </tbody>
             </table>
-            <button onClick={() => history.push({pathname: "/"})}>Upload New Image</button>
         </>
     )
 }
